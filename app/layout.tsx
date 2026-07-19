8397d82db0564d51420fe02058c71da0d3934b87
SamiaBaly <samiabaly116@gmail.com>
2026-07-19 09:22:57 +0600

    feat(frontend): add auth hook and provider setup

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,20 +1,11 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
+import { AuthProvider } from "./providers/AuthProvider";
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
-
-export const metadata = {
+export const metadata: Metadata = {
   title: "CareerPilot AI",
-  description: "AI-powered Career Guidance Platform",
+  description: "AI Powered Career Platform",
 };
 
 export default function RootLayout({
@@ -22,12 +13,18 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+
   return (
-    <html
-      lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
-    >
-      <body className="min-h-full flex flex-col">{children}</body>
+    <html lang="en">
+      <body>
+
+        <AuthProvider>
+
+          {children}
+
+        </AuthProvider>
+
+      </body>
     </html>
   );
 }
