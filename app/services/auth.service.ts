49dafbd388b8b59913a94df87ee9973a8b6073af
SamiaBaly <samiabaly116@gmail.com>
2026-07-19 09:20:01 +0600

    feat(frontend): setup axios and auth service

---
+++ app/services/auth.service.ts
@@ -0,0 +1,44 @@
+import api from "./api";
+
+
+// Register User
+export const registerUser = async (userData: {
+  name: string;
+  email: string;
+  password: string;
+}) => {
+
+  const response = await api.post("/auth/register", userData);
+
+  return response.data;
+};
+
+
+// Login User
+export const loginUser = async (userData: {
+  email: string;
+  password: string;
+}) => {
+
+  const response = await api.post("/auth/login", userData);
+
+  return response.data;
+};
+
+
+// Logout User
+export const logoutUser = async () => {
+
+  const response = await api.post("/auth/logout");
+
+  return response.data;
+};
+
+
+// Get Current User
+export const getCurrentUser = async () => {
+
+  const response = await api.get("/auth/me");
+
+  return response.data;
+};
